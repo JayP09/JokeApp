59d22ee859cc2d3658f0127c01998b6783a4fdee
Jay Panchal <panchalj272@gmail.com>
2022-03-14 14:28:14 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
+import Axios from 'axios';
 
-function App() {
+const TWITTER_HANDLE = 'JayPTwts';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  const [jokeDetails, setJokeDetails] = useState({})
+  const [hex, setHex] = useState("#ffffff");
+
+  const randomColorGenerator = () => {
+      const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      setHex(randomColor)
+  }
+
+  const fetchJokeDetails = async () =>{
+      const response = await Axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist&type=single")
+      console.log(response.data)
+      setJokeDetails(response.data)
+  }
+
+  useEffect(()=>{
+      fetchJokeDetails()
+      randomColorGenerator()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main" style={{backgroundColor: `${hex}`,color: `${hex}`}}>
+      <div className="cardcontainer">
+        <div className="card">
+          <h1>
+              {jokeDetails.category} Joke
+          </h1>
+          <p>
+              {jokeDetails.joke}
+          </p>
+          <button className="btn" onClick={() => {fetchJokeDetails();randomColorGenerator()}} style={{backgroundColor: `${hex}`}}>
+              New Joke
+          </button>
+        </div>
+        <div className="footer" style={{backgroundColor: `${hex}`}}>
+          <p>
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`by Jay`}</a>
+          </p>
+        </div>
+      </div>
     </div>
-  );
+    )
 }
 
 export default App;
